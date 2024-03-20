ae9fd135b3061bd4f3c2e08ce00cc90d373898ac
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-03-21 00:16:58 +0530

    Added utility UI composables

---
+++ theme/src/main/java/com/vinayakgupta3112/minitales/theme/components/MiniTalesPreview.kt
@@ -0,0 +1,16 @@
+package com.vinayakgupta3112.minitales.theme.components
+
+import android.content.res.Configuration
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Preview Day",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "Preview Night",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+annotation class MiniTalesPreview
