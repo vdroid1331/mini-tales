c136402788d15c1bbbbc410f41a9e9234e0f365e
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-03-20 21:59:19 +0530

    Initial Project Setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,67 @@
+import org.gradle.kotlin.dsl.*
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.protobuf)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.vinayakgupta3112.minitales"
+
+    defaultConfig {
+        applicationId = "com.vinayakgupta3112.minitales"
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.androidx.hilt.compose.navigation)
+
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.ktor.client.core)
+
+    implementation(libs.datastore)
+    implementation(libs.protobuf.javalite)
+
+    implementation(libs.timber)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
